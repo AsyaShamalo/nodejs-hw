d26430e759bc548a597da9ece1e9715c88beaccf
asya <shamalo.n.75@gmail.com>
2025-12-04 17:10:43 +0200

    creating branch and doing homework

--- src/models/note.js
+++ src/models/note.js
@@ -1,24 +1,23 @@
-import { Schema } from 'mongoose';
-import { model } from 'mongoose';
+import { Schema, model } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
-    title: {type: String, required: true, trim: true},
-    content: {type: String, required: false, default: '', trim: true},
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    content: {
+      type: String,
+      required: false,
+      default: '',
+      trim: true
+    },
     tag: {
       type: String,
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo'
-      ],
+      required: false,
+      enum: TAGS,
       default: 'Todo'
     },
   },
@@ -27,5 +26,7 @@ const noteSchema = new Schema(
     versionKey: false,
   },
 );
+
+noteSchema.index({ title: 'text', content: 'text' });
 
 export const Note = model('Note', noteSchema);
--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
-
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import { errors } from 'celebrate';
 import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
@@ -18,12 +18,16 @@ app.use(express.json());
 app.use(logger);
 app.use(cors());
 
+
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
+app.use(errors());
 app.use(errorHandler);
 
+
 await connectMongoDB();
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
